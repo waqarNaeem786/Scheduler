c36e8fe2dea659c41f5730d57b43e5a60af4541b
Waqar Naeem <71634063+waqarNaeem786@users.noreply.github.com>
2021-02-03 15:48:34 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <ul>
+        <li className="head-parti">
+          <h3> Participants</h3>
+        </li>
+        <div className="content">
+          <li>user</li>
+          <li>user</li>
+          <li>user</li>
+          <li>user</li>
+          <li>user</li>
+          <li>user</li>
+        </div>
+      </ul>
+      <div className="head">
+        <ul className="date-time">
+          <li className="date">
+            2 Feb 2020
+            <li className="time"> 9am-9pm</li>
+          </li>
+          <li className="date">
+            5 Feb 2020
+            <li className="time"> 9am-9pm</li>
+          </li>
+        </ul>
+      </div>
+      <div className="main">
+        <div className="button">
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
+}
